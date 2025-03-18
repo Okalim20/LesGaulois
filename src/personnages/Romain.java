f82919e30f7d6bfe5b9946200d0c023c08c2541c
Liam Duneau <dnl5504a@ensfsi.onmicrosoft.com>
2025-03-18 16:40:58 +0100

    utilisation des énum

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -1,13 +1,28 @@
 package personnages;
 
+import objets.Equipement;
+
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement[] equipements= new Equipement[2];
+	private int nbEquipement=0;
+	
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
 	}
 	
+	
+	
+	public static void main(String[] args) {
+		System.out.println(Equipement.CASQUE);
+		Romain minus = new Romain("Minus",6);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.BOUCLIER);
+		minus.sEquiper(Equipement.CASQUE);
+	}
 	public String getNom() {
 		return nom;
 	}
@@ -20,6 +35,30 @@ public class Romain {
 		return "Le romain " + nom + " : ";
 	}
 	
+	private void equip(Equipement equipement) {
+		equipements[nbEquipement]=equipement;
+		System.out.println("Le soldat "+nom+" s'équipe avec un "+equipement);
+		nbEquipement+=1;
+	}
+	
+	public void sEquiper(Equipement equipement) {
+		switch (this.nbEquipement) {
+		case 2 :
+			System.out.println("Le soldat "+nom+" est déjà bien équipé");
+			break;
+		case 1 :
+			if (equipements[0]==equipement) {
+				System.out.println("Le soldat "+nom+" possède déjà un "+equipement+" !");
+			}
+			else {
+				equip(equipement);
+			}
+			break;
+		default :
+			equip(equipement);
+		}
+	}
+	
 	
 	public void recevoirCoup(int forceCoup) {
 		force = force - forceCoup;
@@ -29,5 +68,6 @@ public class Romain {
 		else {
 			parler("Aie !");
 		}
+		
 	}
 }
